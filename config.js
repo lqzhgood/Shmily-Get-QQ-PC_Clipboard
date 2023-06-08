aa32965144d333cc3d349f0b3a87aa3344ce8771
lqzh <9134671+lqzhgood@users.noreply.github.com>
2023-06-08 10:26:40 +0800

    🦄 refactor(输出): 规范输出

--- config.js
+++ config.js
@@ -1,16 +1,33 @@
-module.exports = {
-    myName: ['我的昵称1', '我的昵称2'], // 不要有空格
-    herName: ['对方昵称1', '对方昵称2'], // 不要有空格
+const path = require("path");
 
-    rightNum: '110',
-    rightName: 'name~',
+const config = {
+    myName: ["我的昵称1", "我的昵称2"], // 不要有空格
+    herName: ["对方昵称1", "对方昵称2"], // 不要有空格
 
-    leftNum: '119',
-    leftName: 'name',
+    rightNum: "110",
+    rightName: "name~",
 
-    fileBaseInputDir: ['C:\\Users\\%username%\\AppData\\Roaming\\Tencent\\', 'D:\\My bak\\QQ\\110\\'],
+    leftNum: "119",
+    leftName: "name",
 
-    imgWebPublicDir: './data/qq-pc/img',
-    faceWebPublicDir: './data/qq-pc/face',
-    fileWebPublicDir: './data/qq-pc/file',
+    fileBaseInputDir: [
+        "C:\\Users\\%username%\\AppData\\Roaming\\Tencent\\",
+        "D:\\My bak\\QQ\\110\\",
+    ],
+
+    rootPath: "QQ-PC-CP-12345678-20230101",
+
+    DIST_DIR: path.join(__dirname, "./dist/"),
+
+    // imgWebPublicDir: "./data/qq-pc/img",
+    // faceWebPublicDir: "./data/qq-pc/face",
+    // fileWebPublicDir: "./data/qq-pc/file",
 };
+
+config.DIST_DIR_TEMP = path.join(config.DIST_DIR, "_temp");
+
+config.imgWebPublicDir = `./data/${config.rootPath}/img`;
+config.faceWebPublicDir = `./data/${config.rootPath}/face`;
+config.fileWebPublicDir = `./data/${config.rootPath}/file`;
+
+module.exports = config;
--- msg.js
+++ msg.js
@@ -1,17 +1,18 @@
-const fs = require('./lib/fs');
+const fs = require("./lib/fs");
 
-const { fileToNodeList } = require('./lib/utils');
-const { getPart } = require('./lib/type');
+const { rootPath } = require("./config");
+const { fileToNodeList } = require("./lib/utils");
+const { getPart } = require("./lib/type");
 
-console.log('start');
+console.log("start");
 
 const nodeList = fileToNodeList();
 
 // 这个必须在最后 因为 nodeList 会被裁剪
 const msgArr = getPart(nodeList);
 
-fs.mkdirpSync('./dist');
+fs.mkdirpSync("./dist");
 
-fs.writeFileSync(`./dist/qq_rich_copy_msg.json`, JSON.stringify(msgArr, null, 4));
+fs.writeFileSync(`./dist/${rootPath}.json`, JSON.stringify(msgArr, null, 4));
 
-console.log('ok');
+console.log("ok");
